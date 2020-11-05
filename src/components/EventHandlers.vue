b4217de49e23818cfda93f1a0afba8e2e0661ff0
sellandula-nisum-com <sellandula@nisum.com>
2020-11-05 05:27:35 -0800

    [FEATURE]: Vue JS POC

---
+++ src/components/EventHandlers.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+         <h1>
+            {{newComponent}}
+        </h1>
+        <div>
+           <button v-on:click="counter += 1">Add 1</button>
+            <p>The button above has been clicked {{ counter }} times.</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "EventHandlers",
+    props: {
+        newComponent: String
+    },
+    data : () => {
+       return { counter: 0 }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }} (This Heading has been sent from other component as props) </h1>
+     <div v-if="true">
+        Usage of v-if directory.
+    </div>
+    <div v-show="true">
+        Usage of v-show directory.
+    </div>
+
+    <span>Message: {{sampleMessage}}</span>
+
+    <p> Displaying raw HTML : <i><b> {{ rawHtml }}</b></i></p>
+    <p>Using v-html directive dispalying executable HTML: <span v-html="rawHtml"></span></p>
+
+   <div v-bind:id="dynamicId()">Usage of v-bind </div>
+  </div>
+  
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data : () => { 
+    return {
+        sampleMessage: "This is the sameple message",
+        rawHtml: "<h2 style='color:green'>RAW HTML</h2>",
+        dynamicId: () => {
+          return "bindId";
+        }
+    }
+  }
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.hello {
+  text-align: center;
+}
+</style>
